2374d081c4f68b71ff6eca946545a4f446da311b
bazengo <61855206+bazengo@users.noreply.github.com>
2025-01-26 14:53:48 -0700

    stop

--- ggj2025/bubble_length.gd
+++ ggj2025/bubble_length.gd
@@ -1,15 +1,17 @@
 extends Timer
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-
-func _on_stockprice_bubblefactor() -> void:
+signal bubble_in_progress
+func _on_stockprice_bubblefactor(i) -> void:
+	wait_time = i
+	print(wait_time)
+	bubble_in_progress.emit()
+	start()
 	pass # Replace with function body.
