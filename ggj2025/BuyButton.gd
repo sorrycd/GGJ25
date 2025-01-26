b119642f7b7511a438ed1f17f8d728d3246092a3
bazengo <61855206+bazengo@users.noreply.github.com>
2025-01-25 17:43:30 -0700

    :3c

--- ggj2025/BuyButton.gd
+++ ggj2025/BuyButton.gd
@@ -1,5 +1,4 @@
 extends Button
-var price = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,10 +8,3 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-	
-
-
-func _on_stockprice_buy_price(i) -> void:
-	price = i
-	
-	pass # Replace with function body.
--- ggj2025/holdings.gd
+++ ggj2025/holdings.gd
@@ -13,19 +13,26 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-func _on_buy_button_pressed(i) -> void:
-	pass # Replace with function body.
 	
 func _on_stockprice_market_changed(i) -> void:
 	stockprice = i[-1]
 	print('help',i[-1])
 	print('holdings',stockprice)
-
 signal sell_asset_data
+signal buy_asset_data
+func _on_buy_button_pressed() -> void:
+	print('BUYBUYBUY')
+	if liquid > stockprice:
+		stock_holdings += 1
+		liquid -= stockprice
+		sell_asset_data.emit([stock_holdings,liquid])
+	pass # Replace with function body.
+
+
 func _on_sell_button_pressed() -> void:
 	if stock_holdings > 0:
 		stock_holdings -=1
 		liquid += stockprice
-		print('StockPrice:',stockprice)
+		#print('StockPrice:',stockprice)
 		sell_asset_data.emit([stock_holdings,liquid])
 	pass # Replace with function body.
