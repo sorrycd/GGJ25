77a1f51f749326db4b72518f015add2968c81f6a
sorrycd <coleson.dyck@gmail.com>
2025-01-25 17:44:21 -0700

    title screen changes once more

--- ggj2025/title_screen/texture_button.gd
+++ ggj2025/title_screen/texture_button.gd
@@ -11,15 +11,3 @@ func _on_mouse_exited():
 
 func wobble_button():
 	# Reset rotation
-	rotation_degrees = 0
-
-	# Create the tween
-	var tween = get_tree().create_tween()
-
-	# Wobble effect: Rotate to 10 degrees, -10 degrees, and back to 0
-	tween.tween_property(self, "rotation_degrees", 10, 0.1).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
-	tween.tween_property(self, "rotation_degrees", -10, 0.2).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
-	tween.tween_property(self, "rotation_degrees", 0, 0.1).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
-
-	# Optional: Add a loop if you want the wobble to continue until the mouse leaves
-	# tween.set_loops(true)
--- ggj2025/title_screen/title_screen.gd
+++ ggj2025/title_screen/title_screen.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@onready var button: Button = $CanvasLayer/Control/Button
+@onready var button: TextureButton = $CanvasLayer/Control/TextureButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
