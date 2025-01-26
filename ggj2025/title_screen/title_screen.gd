845d4f975116784dfd5355fa4dcac7f677cef062
sorrycd <coleson.dyck@gmail.com>
2025-01-26 15:58:44 -0700

    win screen

--- ggj2025/title_screen/title_screen.gd
+++ ggj2025/title_screen/title_screen.gd
@@ -12,3 +12,8 @@ func _ready() -> void:
 func _on_texture_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://stock_ticker.tscn")
 	pass # Replace with function body.
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("reset_title"):
+		get_tree().change_scene_to_file("res://title_screen/Title_Screen.tscn")
+	pass
