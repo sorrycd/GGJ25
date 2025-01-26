729195a15a5324a6cd1aa08cbf32c0dd6bb42e8c
sorrycd <coleson.dyck@gmail.com>
2025-01-26 03:43:53 -0700

    some changes, reset function implemented

--- ggj2025/Main.gd
+++ ggj2025/Main.gd
@@ -9,3 +9,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("reset_title"):
+		get_tree().change_scene_to_file("res://title_screen/Title_Screen.tscn")
+	pass
