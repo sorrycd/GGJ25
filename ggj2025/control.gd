aa5a8a678f4af658bb3dfc3a7ab4e2934b3e2d02
sorrycd <coleson.dyck@gmail.com>
2025-01-26 15:56:25 -0700

    button sounds

---
+++ ggj2025/control.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_buy_pressed() -> void:
+	$Click1.play()
+	pass # Replace with function body.
+
+func _on_sell_pressed() -> void:
+	$Click2.play()
+	pass # Replace with function body.
